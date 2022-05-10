af8c7d12e28ad4e31d14abc1aefa4f35aeaa7546
csengekulker <k71608883429@ktch.hu>
2022-05-10 08:30:52 +0200

    MainFrame constructor and method templates

---
+++ src/views/MainFrame.java
@@ -0,0 +1,24 @@
+package views;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+  public MainFrame() {
+
+    setComponents();
+    setMainPanel();
+    setFrame();
+  }
+
+  private void setComponents() {
+
+  }
+
+  private void setMainPanel() {
+
+  }
+
+  private void setFrame() {
+
+  }
+}
